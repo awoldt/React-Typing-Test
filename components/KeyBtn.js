055e0cfee7e3a67a3ee124e6b2a8baee75f45fa2
awoldt <abreckwoldt23@gmail.com>
2021-12-14 20:48:31 -0500

    keypress makes key darker and lighter when keyup

--- components/KeyBtn.js
+++ components/KeyBtn.js
@@ -1,13 +1,39 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef} from "react";
 
 const KeyBtn = ({ keyValue, color }) => {
- 
+
+  const keyRef = useRef();
+
+
+  function keyPress(x) {
+    //blue
+    if (x === "rgb(0, 102, 255)") {
+      keyRef.current.style.backgroundColor = 'rgb(0, 55, 138)'
+    }
+    //red
+    if (x === "rgb(179, 0, 0)") {
+      keyRef.current.style.backgroundColor = 'rgb(125, 0, 0)'
+    }
+  }
+
+  function keyLift(x) {
+    //blue
+    if (x === "rgb(0, 102, 255)") {
+      keyRef.current.style.backgroundColor = 'rgb(0, 102, 255)'
+    }
+    //red
+    if (x === "rgb(179, 0, 0)") {
+      keyRef.current.style.backgroundColor = 'rgb(179, 0, 0)'
+    }
+  }
 
   return (
     <div
-      style={{ userSelect: "none", backgroundColor: color, height: "100%"}}
-      
+      style={{ userSelect: "none", backgroundColor: color, height: "100%" }}
+      onMouseDown={() => {keyPress(color)}}
+      onMouseUp={() => {keyLift(color)}}
+      ref={keyRef}
     >
       {keyValue}
     </div>
